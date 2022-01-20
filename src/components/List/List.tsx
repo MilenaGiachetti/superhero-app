321f04c1677dd8f6ce5548ad0e1f236f1374cc6a
Milena Giachetti <giachettimilena@gmail.com>
2022-01-20 20:11:55 -0300

    Add List component

---
+++ src/components/List/List.tsx
@@ -0,0 +1,33 @@
+import styled from 'styled-components';
+
+const ListContainer = styled.ul`
+	padding-left: 10px;
+`
+
+const ListItem = styled.li`
+	position: relative;
+	text-transform: capitalize;
+	&:before {
+		background-color: #CCC;
+		border-radius: 1px;
+		content: '';
+		display: block;
+		height: 1px;
+		left: -10px;
+		position: absolute;
+		top: 16px;
+		width: 5px;
+	}
+`
+
+function List({array}) {
+  return (
+    <ListContainer>
+        { array.map((el, i) => {
+            return <ListItem key={i}>{el}</ListItem>
+        })}
+    </ListContainer>
+  )
+}
+
+export default List;
