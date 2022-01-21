f9f7dbf109f8de973671e9904bd1540a2024ab30
Milena Giachetti <giachettimilena@gmail.com>
2022-01-21 13:02:13 -0300

    Add search base

--- src/views/Home.tsx
+++ src/views/Home.tsx
@@ -1,10 +1,12 @@
-import { useEffect, useState } from 'react';
+import { useCallback, useEffect, useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 import axios from 'axios';
 import styled from 'styled-components';
 import GridItem from '../components/GridItem/GridItem';
 import Pagination from '../components/Pagination/Pagination';
 import { useQuery } from '../hooks/useQuery';
 import { Hero } from '../types/hero.types';
+import NotFound from '../components/NotFound/NotFound';
 
 const Grid = styled.ul`
 	display: grid;
@@ -26,38 +28,71 @@ const Grid = styled.ul`
 `;
 
 const Home = () => {
-	let query = useQuery().get('page');
-	let pageQuery = query ? +query : 1;
+	let queries = useQuery();
+	let pQuery = queries.get('page');
+	let sQuery = queries.get('search');
+	const navigate = useNavigate();
+	let pageQuery = pQuery ? +pQuery : 1;
+	const [search, setSearch] = useState<string>(sQuery ? sQuery : '');
 	const [heroes, setHeroes] = useState<Hero[]>([]);
+	const [filteredHeroes, setFilteredHeroes] = useState<Hero[]>([]);
 	const elementsByPage = 24;
+
+	const getFiltered = useCallback((data) => {
+		if (search.trim()) {
+			const filtered = data.filter(({name}) => {
+				const re = new RegExp(search, "i")
+				return name.match(re)
+			})
+			navigate(`?page=1&search=${search}`)
+			setFilteredHeroes(filtered);
+		} else if (!search.trim()) {
+			navigate(`?page=1&search=${search}`)
+			setFilteredHeroes(data);
+		}
+	}, [navigate, sQuery, search]);
+	
 
 	useEffect(() => {
 		axios.get('https://akabab.github.io/superhero-api/api/all.json')
-		.then(response => {
-			setHeroes(response.data);
+		.then(({data}) => {
+			setHeroes(data);
+			getFiltered(data);
 		})
 		.catch(error => {
 			console.log(error)
 		})
-	}, [])
+	}, [getFiltered])
 
 	useEffect(() => {
 		window.scrollTo({top: 0, behavior: 'smooth'});
 	}, [pageQuery])
 
+	useEffect(() => {
+		let timer = setTimeout(() => {
+			getFiltered(heroes);
+		}, 1000)
+		return () => clearTimeout(timer);
+	}, [getFiltered])
+
 	return (
 		<>
-			<Grid>
+			<input type='search' value={search} onChange={(e) => setSearch(e.target.value)}></input>
 				{
-					heroes.map.length &&
-					heroes.slice(elementsByPage * (pageQuery - 1), elementsByPage * pageQuery).map(hero => {
-						return (
-							<GridItem hero={hero} key={hero.id} />
-						)
-					})
+					filteredHeroes.length ?
+					<Grid>
+						{ 
+							filteredHeroes.slice(elementsByPage * (pageQuery - 1), elementsByPage * pageQuery).map(hero => {
+								return (
+									<GridItem hero={hero} key={hero.id} />
+								)
+							})
+						} 
+					</Grid>
+					:
+					<NotFound>Hero '{search}' not found</NotFound>
 				}
-			</Grid>
-			<Pagination total={heroes.length} currentPage={pageQuery} elementsByPage={elementsByPage} />
+			<Pagination total={filteredHeroes.length} currentPage={pageQuery} elementsByPage={elementsByPage} search={search} />
 		</>
 	)
 }
